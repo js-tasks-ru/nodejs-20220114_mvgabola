e91d43c95c89b0ab6f4a49a58980bfc45251182b
ritagabo <mglovely@ya.ru>
2022-01-19 14:06:34 +0300

    Решение 01-intro/01-sum Сумма двух чисел

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,12 @@
 function sum(a, b) {
-  /* ваш код */
+  if (isNumber(a) && isNumber(b)) {
+    return a + b;
+  }
+  throw new TypeError('один или оба аргумента не являются числом');
+}
+
+function isNumber(number) {
+  return Number.isFinite(number);
 }
 
 module.exports = sum;
