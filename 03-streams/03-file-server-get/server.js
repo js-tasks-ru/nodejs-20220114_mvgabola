ff9485cd2f2c333398a76ab0b0c71667a82d4b6a
ritagabo <mglovely@ya.ru>
2022-01-30 17:47:27 +0300

    solution for  03-streams/03-file-server-get Файловый сервер - отдача файла

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,18 +1,30 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+server.on('request', async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
-
   switch (req.method) {
     case 'GET':
-
+      if (pathname.match(/\//g)) {
+        res.statusCode = 400;
+        res.end();
+      }
+      const stream = fs.createReadStream(filepath);
+      stream.on('error', (error)=>{
+        if (error.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end();
+        }
+        res.statusCode = 500;
+        res.end();
+      });
+      stream.pipe(res);
       break;
 
     default:
