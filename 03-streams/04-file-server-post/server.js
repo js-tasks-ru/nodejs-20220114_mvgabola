33284970d2d6ba72764100acbd6b459697e577fc
ritagabo <mglovely@ya.ru>
2022-02-01 22:41:55 +0300

    add solution to 03-streams/04-file-server-post Файловый сервер - запись файла

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,6 +1,11 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const {access} = require('fs/promises');
+const {pipeline} = require('stream');
+
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -9,12 +14,39 @@ server.on('request', (req, res) => {
   const pathname = url.pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
-
+  if (pathname.match(/\//g)) {
+    res.statusCode = 400;
+    res.end();
+  }
   switch (req.method) {
     case 'POST':
-
+      const limitedStream = new LimitSizeStream({limit: 100000, encoding: 'utf-8'});
+      access(filepath)
+          .then(()=>{
+            res.statusCode = 409;
+            res.end('Файл уже существует');
+          })
+          .catch((error)=>{
+            if (error.code === 'ENOENT') {
+              const stream = fs.createWriteStream(filepath);
+              pipeline(req,
+                  limitedStream,
+                  stream,
+                  (err)=>{
+                    if (err) {
+                      if (err.code === 'LIMIT_EXCEEDED') {
+                        res.statusCode = 413;
+                      }
+                      fs.unlink(filepath, ()=> {});
+                      res.end();
+                    } else {
+                      res.statusCode = 201;
+                      res.end();
+                    }
+                  });
+            }
+          });
       break;
-
     default:
       res.statusCode = 501;
       res.end('Not implemented');
